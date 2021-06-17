121175ea457486668286ea473b24cbe373f5920c
Devansh Kaushik <61897385+Dk-0821@users.noreply.github.com>
2021-06-17 22:11:11 +0530

    Update AIdoctor.py

--- AIdoctor.py
+++ AIdoctor.py
@@ -8,7 +8,7 @@ import webbrowser
 import re
 import pygame
 
-serpwow = GoogleSearchResults("get your id")
+serpwow = GoogleSearchResults("Get Your key")
 engine = pyttsx3.init()
 m = sr.Recognizer()
 ph = sr.Recognizer()
@@ -31,19 +31,13 @@ i5 = ImageTk.PhotoImage(Image.open('C:/Users/DEVANSH/Desktop/project-files/5.jpe
 i6 = ImageTk.PhotoImage(Image.open('C:/Users/DEVANSH/Desktop/project-files/6.jpeg'))
 
 
-def restart():
-    mental()
-
-
 
 def psychosocial():
     n2 = e1.get()
-    b1.place_forget()
     b2.place_forget()
     engine.setProperty('rate', 130)
-    engine.say(" Hi " + n2 + "Relax Your Mind Through this small session")
-    engine.runAndWait()
-    engine.setProperty('rate', 130)
+    engine.say(" Hi " + n2 )
+    engine.say("Relax Your Mind Through this small session")
     engine.say(" Lets Perfome A White Light Session for you")
     engine.runAndWait()
     pygame.init()
@@ -51,8 +45,8 @@ def psychosocial():
     pygame.mixer.music.load("C:/Users/DEVANSH/Desktop/project-files/in-the-light.mp3")
     pygame.mixer.music.set_volume(0.2)
     pygame.mixer.music.play()
-    k1 = Label(root,width=1500, height=900, bg='black')
-    k1.pack(ipadx=10, ipady=0, fill=BOTH, expand=True)
+    k1 = Label(root,width=760, height=900, bg='black')
+    k1.place(x=760, y=0)
     k1.configure(image=i1)
     root.update()
     root.after(500, None)
@@ -60,7 +54,6 @@ def psychosocial():
     engine.say("Imagin  A stream of vibrant, white light flows infinitely down from Source and in through the top of your head.\
                Feel the light wash over you as a purifying waterfall, cleansing all past worries, fears, and anxieties.\
                The light flows through you as you experience unconditional Love, joy, well-being, and vitality all in the name of the Creator")
-    engine.runAndWait()
     engine.say("The white light shines vibrantly, as vibrant as all the jewels and gems in this world, and reflects all the colors of the rainbow throughout.\
                Feel each colour strike each individual cell of your body with radiating love and nourishment .")
     engine.runAndWait()
@@ -69,9 +62,7 @@ def psychosocial():
     k1.configure(image=i2)
     root.update()
     engine.setProperty('rate', 87)
-    engine.runAndWait()
     engine.say("YOU ARE HEALING.")
-    engine.runAndWait()
     engine.say(" ")
     engine.runAndWait()
     engine.say("The stream of light flows through your body enrichening and enlivening you.\
@@ -83,13 +74,11 @@ def psychosocial():
     k1.configure(image=i3)
     root.update()
     engine.setProperty('rate', 87)
-    engine.runAndWait()
     engine.say("From your crown chakra, Breathe the white light deeper into your 3rd eye chakra filling it with Love and Light.\
                 Reach out and request the white light to grant you clairvoyance to your inner vision and allow your perspective to expand infinitely.\
                 From here, breathe the white light down further into your throat chakra. \
                Your place of speaking where Truth is uttered. \
                Breathe out the white light it passes through your throat chakra and releases the Truth into the universe through your voice.")
-    engine.runAndWait()
     engine.say("Next, move the white light down into you heart chakra with your breath.\
                Feel the ultimate expansion at an infinite rate as pure white light pierces and becoyous every aspect of your heart.\
                Your heart now radiates as a brilliant white sphere of Light and Love.")
@@ -100,8 +89,6 @@ def psychosocial():
     root.update()
     engine.setProperty('rate', 87)
     engine.say("You are Light.")
-    engine.runAndWait()
-    engine.runAndWait()
     engine.say("")
     engine.say("Bring the healing white light down through your crown chakra and into you solar plexus chakra, just below the chest but above the belly.\
                release youself from all limitations and expectations placed upon you externally.\
@@ -115,12 +102,9 @@ def psychosocial():
     engine.say("Now breathe this pure, white light down from your crown chakra and into you sacral chakra, just below the navel.\
                Breathe deeply into this space.\
                This is the space you were born from. How you entered into this world.")
-    engine.runAndWait()
-    engine.say("")
-    engine.runAndWait()
+    engine.say(" ")
     engine.say("Surrender all things that no longer serve you .")
-    engine.runAndWait()
-    engine.say("")
+    engine.say(" ")
     engine.runAndWait()
     root.after(500, None)
 
@@ -130,9 +114,7 @@ def psychosocial():
     engine.say("Now,  breathe the pure, healing, white light down into you root chakra.\
                This grounds you within this reality and provides you with a strong foundation in which to operate from.\
                Feel an ancient connection to Mother Earth and all of your fellow humans.")
-    engine.runAndWait()
     engine.say("")
-    engine.runAndWait()
     engine.say("You are pure, white light.")
     engine.runAndWait()
     pygame.mixer.music.stop()
@@ -142,49 +124,71 @@ def psychosocial():
     screen1()
 
 
-def mental():
-    n1 = e1.get()
-    b1.place_forget()
-    b2.place_forget()
-    c1 = Canvas(root)
-    c1.create_image(700, 430, image=img2)
-    c1.pack(ipadx=0, ipady=0, fill=BOTH, expand=TRUE)
-    b4 = Button(root, text="Once's more", width=15, height=2, bg="light blue", font=("Playfair Display", 20), fg="black"
-                , command=restart)
-    b4.place(x=650, y=550)
-    d1 = Label(root)
-    d1.place(x=0, y=100)
-    IPaddress = socket.gethostbyname(socket.gethostname())
-    if IPaddress == "127.0.0.1":
-        engine.say(n1 + "Please connect to an network")
-        engine.runAndWait()
-    else:
-        root.after(2000, None)
-        root.update()
-        engine.setProperty('rate', 150)
-        engine.say("Hi" + n1 + " i am here to help your mentally." + " take me as your close friend")
-        engine.runAndWait()
-        engine.say("Just call Amigo and i will be there for you")
-        engine.runAndWait()
-        run = True
-        while run:
-            try:
+
+def screen1():
+    global b1, b2
+    c.pack_forget()
+
+    def mental():
+        global repeat
+        n1 = e1.get()
+        b1.pack_forget()
+        lab = Label(root, width=760, height=900, image=img2)
+        lab.place(x=-4, y=0)
+        '''b1.configure(image=img2);
+        b4 = Button(root, text="Once's more", width=15, height=2, bg="light blue", font=("Playfair Display", 20), fg="black"
+                    , command=restart)
+        b4.place(x=650, y=550)#3b3068'''
+        d1 = Text(root, width=42, height=18.4, background="#3b3068", foreground="#f96bc0",
+                  font=("Playfair Display", 20))
+        d1.place(x=56, y=144)
+        IPaddress = socket.gethostbyname(socket.gethostname())
+        if IPaddress == "127.0.0.1":
+            d1.insert(END, "\n\n\n\n\n\n\n       "
+                           "NOT CONNECTED TO INTERNET :)\n         "
+                           "Please check your internet connectivity \n      "
+                           "Then i am ready to help you[--->  /^!^\ <---]")
+            root.update()
+            engine.say(n1 + "You are not connect to a netork")
+            engine.say(n1 + "I will be ready as you connect to a network")
+            engine.runAndWait()
+            mental()
+        else:
+            root.after(2000, None)
+            d1.insert(END, "\n\n\n\n\n\n\n       "
+                      "   Hi " + n1 + " myself Amigo and i am here to help your\n              "
+                                           " Take me as your close friend\n            "
+                                           "   To exit say --> (GoodBye) ")
+            root.update()
+            engine.setProperty('rate', 150)
+            engine.say("Hi " + n1 + " Myslef Amigo and i am here to help your")
+            engine.say("Take me as your close friend     ")
+            engine.runAndWait()
+            run = True
+            while run:
+                d1.delete("1.0", "end")
+                d1.insert(END, "\n\n\n\n\n\n\n\n             "
+                               "      How can i help you     ")
+                root.update()
+                engine.say('How can i help you' + n1)
+                engine.runAndWait()
                 with sr.Microphone() as source:
+                    print("AMIGO:")
                     audio = m.listen(source)
                 try:
                     print(m.recognize_google(audio))
-                    t = m.recognize_google(audio)
-                    if t == 'Amigo':
-                        engine.say('How can i help you' + n1)
+                    l = m.recognize_google(audio)
+                    if l == "goodbye":
+                        d1.delete("1.0", "end")
+                        d1.insert(END, "\n\n\n\n\n\n\n\n             "
+                                       " Goodbye be happy and have a nice day     ")
+                        root.update()
+                        engine.say('Goodbye')
+                        engine.say('Be happy and have a nice day' + n1)
                         engine.runAndWait()
-                        with sr.Microphone() as source:
-                            print("AMIGO:")
-                            audio = m.listen(source)
-                        try:
-                            print(m.recognize_google(audio))
-                            l = m.recognize_google(audio)
-                        except:
-                            print(" ")
+                        screen1()
+
+                    def repeat():
                         params = {
                             "q": l,
                             "gl": "in",
@@ -194,37 +198,28 @@ def mental():
                             "device": "desktop",
                             "csv_fields": "related_questions.answer,related_questions.source.link"
                         }
-                        if l == "goodbye":
-                            screen1()
-                        else:
-                            # retrieve the search results as CSV
-                            result = serpwow.get_csv(params)
-                            urls = re.findall(
-                                'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
-                                result[58:None]
-                            )
+                        result = serpwow.get_csv(params)
+                        urls = re.findall(
+                            'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+                            result[58:None]
+                        )
+                        text = str(result[58:None])
+                        d1.delete("1.0", "end")
+                        d1.insert(END, text)
+                        root.update()
+                        if urls:
+                            RU = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', result[58:None])
+                            engine.setProperty('rate', 145)
+                            engine.say("Here is your solution to your problem " + n1 + " " + RU)
+                            engine.runAndWait()
+                        for i in range(len(urls)):
+                            webbrowser.open(str(urls[i]))
 
-                            d1.configure(text=str(result[58:None]))
-                            if urls:
-                                RU = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', result[58:None])
-                                engine.setProperty('rate', 145)
-                                engine.say("Here is your solution to your problem " + n1 + " " + RU)
-                                engine.runAndWait()
-                            for i in range(len(urls)):
-                                webbrowser.open(str(urls[i]))
+                    run = False
+                    repeat()
+                except:
+                    print(" ")
 
-                except sr.UnknownValueError:
-                    print("Could not understand audio")
-                    mental()
-            except sr.RequestError as e:
-                print("Could not request results; {0}".format(e))
-
-            run = False
-
-
-def screen1():
-    global b1, b2
-    c.pack_forget()
     b1 = Button(root, text="Mental health", width=760, height=900, image=img1, command=mental)
     b1.place(x=-4, y=0)
     b2 = Button(root, text="Physocial support", width=760, height=900, image=img3, command=psychosocial)
